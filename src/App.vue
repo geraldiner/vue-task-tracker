7e4ee8458e35027572695fde6be5340624a5e597
Geraldine <hello@geraldiner.com>
2022-02-20 20:26:12 -0500

    add: implement vue-router for an about page. set up conditionals for what gets show on home vs about

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,31 @@
 <template>
   <div class="container">
     <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
-    <div v-show="showAddTask">
-      <AddTask @add-task="addTask" />
 
-    </div>
-    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header"
-import Tasks from "./components/Tasks.vue"
-import AddTask from "./components/AddTask"
+import Footer from "./components/Footer"
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks,
-    AddTask
+    Footer
   },
   data() {
     return {
-      tasks: [],
       showAddTask: false
     }
   },
   methods: {
-
-    async toggleReminder(id) {
-      const taskToToggle = await this.fetchTask(id);
-      const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
-
-      const res = await fetch(`api/tasks/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updatedTask)
-      })
-
-      const data = await res.json();
-
-      this.tasks = this.tasks.map(task => {
-        return task.id === id ? {...task, reminder: data.reminder} : task;
-      })
-    },
-    async deleteTask(id) {
-      if(confirm("Are you sure?")) {
-        const res = await fetch(`api/tasks/${id}`, {
-          method: 'DELETE'
-        })
-
-        res.status === 200 ? (this.tasks = this.tasks.filter(task => task.id !== id)) : alert("Error deleting task");
-      }
-    },
-    async addTask(newTask) {
-      const res = await fetch('api/tasks/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newTask)
-      })
-      const data = await res.json();
-      this.tasks = [...this.tasks, data];
-    },
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
-    },
-    async fetchTasks() {
-      const res = await fetch('api/tasks');
-      const data = await res.json();
-      return data;
-    },
-    async fetchTask(id) {
-      const res = await fetch(`api/tasks/${id}`);
-      const data = await res.json();
-      return data;
     }
-  },
-  async created() {
-    this.tasks = await this.fetchTasks();
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <p>Copright &copy; 2022</p>
+    <router-link v-show="aboutPage" to="/about">About</router-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  computed: {
+    aboutPage() {
+      return this.$route.path !== '/about';
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #333;
+}
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
